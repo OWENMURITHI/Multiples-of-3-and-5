5dcc24d2bc8e004d6800ba5a1f8824c99b7029f4
Owen Murithi <eng.owenmuriithi@gmail.com>
2021-04-26 18:10:34 +0300

    Update main.py
    Ask for the minimum range and maximum range

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 total = 0
 
-maxrange = input("Enter the maximum range") #Get the maximum range from the keyboard
+minRange = input("Enter the minimum range")
+maxRange = input("Enter the maximum range") #Get the maximum range from the keyboard
 print("")
 
-max = int(maxrange)
+min = int(minRange)
+max = int(maxRange)
 
-for i in range (0,max):
+for i in range (min,max):
     if i%3 == 0 or i%5 ==0:
         total = total +i
 
